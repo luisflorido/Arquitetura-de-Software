00f8bd234a6816e1c5e28c523e003fb8125c48da
Luis Guilherme <luiisflorido@gmail.com>
2020-03-12 22:54:42 -0300

    code

--- ProjectSecomp/src/main/java/BancoSecomp.java
+++ ProjectSecomp/src/main/java/BancoSecomp.java
@@ -4,44 +4,68 @@ import java.util.Scanner;
 
 
 public class BancoSecomp {
-     String nome;
-    ArrayList<String> lista = new ArrayList(); // Inicialização de um array que 
+  /*
+    1 - encapsulamento
+   */
+//     String nome;
+    private String nome;
+    /*
+      1 - encapsulamento
+     */
+//    ArrayList<String> lista = new ArrayList(); // Inicialização de um array que
     // chamado lista onde ficará armazenado os nomes das pessoas do evento.
-    
+    private ArrayList<String> lista = new ArrayList<>();
+
+
     public String leitura(){ // Função de leitura.
-        
+
         Scanner leitor = new Scanner(System.in);
         String pessoa = leitor.nextLine();
+        leitor.close();
         return pessoa;
     }
-    
+
+    private String formatString(String s){
+      return s.replaceAll(" ", "");
+    }
+
     public void cadastro(String pessoa){
-        
-        int repete = 0; // Variável para verificar se um nome foi repetido ou 
+        /*
+        variável não utilizada
+         */
+//        int repete = 0; // Variável para verificar se um nome foi repetido ou
         // não. Caso for 1, o nome é repetido, caso for 0 o nome serpa inserido
         // na lista.
+
         int tamanho = pessoa.length(); // Tamanho da lista em números.
-        
+
          if (tamanho == 0){
                 System.out.println("Digite o nome da pessoa");
                 cadastro(leitura());
-            } 
-                
+            }
+
        for (int i = 0; i < lista.size(); i++) { // For para validar se um nome
            //é repetido ou não.
-            if (lista.get(i).equals(pessoa) ) {
+         
+         /*
+            1 - organizar estrutura
+            2 - legibilidade
+            3 - verificação melhor dos nomes
+          */
+//         if (lista.get(i).equals(pessoa) ) {
+            if (formatString(lista.get(i)).equalsIgnoreCase(formatString(pessoa))) {
                 System.out.println("\nNome repetido");
                 return;
             }
         }
         lista.add(pessoa); // Adiciona a pessoa na lista do evento.
     }
-    
+
     public void imprimirLista(){ // Função para impressão da lista de pessoas
         // do evento.
-        
+
         for(String nome : lista){
             System.out.println(nome);
         }
-    } 
+    }
 }
--- ProjectSecomp/src/main/java/Main.java
+++ ProjectSecomp/src/main/java/Main.java
@@ -3,35 +3,55 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
-        BancoSecomp banco = new BancoSecomp(); // Instanciação da classe Banco
-        //Secomp
-        String x = new String(); // Variável para armazenar leitura da opção no
-        // menu.
-        
-        while(true){
 
-            System.out.println("1 - Incluir um nome");
-            System.out.println("2 - Listar nomes\n");
-            Scanner leitor = new Scanner(System.in); 
-            x = leitor.nextLine();
 
-            switch(x){
-                
-                case "1":
-                    String nome;
-                    nome = banco.leitura();
-                    banco.cadastro(nome);
-                    break;
-                
-                case "2":
-                    banco.imprimirLista();
-                    break;
-                
-                default:
-                    System.out.println("Valor inválido");
-                    break;
-            }     
-        }   
+  private static BancoSecomp banco;
+
+  public static void main(String[] args) {
+    banco = new BancoSecomp();
+      /*
+        1 - encapsular
+        2 - organizar estrutura
+         */
+    //BancoSecomp banco = new BancoSecomp(); // Instanciação da classe Banco |
+    //Secomp
+    // colocando o menu dentro de um método para organização, legibilidade e acesso
+    /*
+     1 - utilizar mais métodos
+     */
+    menu();
+  }
+
+  private static void menu() {
+    String x = new String(); // Variável para armazenar leitura da opção no
+    // menu.
+    /*
+      1 - melhor utilização das bibliotecas (da forma que estava, o scanner era realocado a toda iteração
+     */
+    Scanner leitor = new Scanner(System.in);
+    while (true) {
+
+      System.out.println("1 - Incluir um nome");
+      System.out.println("2 - Listar nomes\n");
+//      Scanner leitor = new Scanner(System.in);
+      x = leitor.nextLine();
+
+      switch (x) {
+        case "1":
+          String nome;
+          nome = banco.leitura();
+          banco.cadastro(nome);
+          break;
+
+        case "2":
+          banco.imprimirLista();
+          break;
+
+        default:
+          System.out.println("Valor inválido");
+          break;
+      }
+      leitor.close();
     }
+  }
 }
